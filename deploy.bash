4b67db7911b74af49866cb6d3263278cf0d146d4
Tim Visher <tim.visher@gmail.com>
2009-02-26 14:34:14 -0500

    Fixed deploy script. EOM

--- deploy.bash
+++ deploy.bash
@@ -5,8 +5,13 @@ if [ -f ~/.bashrc ]; then
 fi
 
 if [ -f ~/.bash_profile ]; then
-    rm ~/.bash_profile ]
+    rm ~/.bash_profile
+fi
+
+if [ -f ~/.inputrc ]; then
+    rm ~/.inputrc
 fi
 
 cp bashrc ~/.bashrc
 cp bash_profile ~/.bash_profile
+cp inputrc ~/.inputrc
