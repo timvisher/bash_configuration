dd5dd3faeba6f86c56ba9067fbf97e291faad606
Tim Visher <tvisher@monetate.com>
2013-08-14 15:54:33 -0400

    Upgrade some of my shell scripts. EOM

---
+++ bin/esv.to.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'rubygems'
+require 'open-uri'
+require 'clipboard'
+
+# require 'ruby-debug';debugger;1
+if ARGV.length == 0
+  copystr = 'http://esv.to/' + URI::encode(Clipboard.paste)
+else
+  copystr = 'http://esv.to/' + URI::encode(ARGV.join(' '))
+end
+
+Clipboard.copy copystr
+puts '"' + copystr + '" is all up in your clipboard'
+
---
+++ bin/google lucky insert.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'open-uri'
+require 'net/http'
+
+query_url = 'http://www.google.com/search?q=%s&btnI'
+query = ARGV.length > 0 ? URI::encode(ARGV.join ' ') : URI::encode(ARGF.read)
+
+uri =      URI::parse(sprintf(query_url, query))
+http =     Net::HTTP.new(uri.host, uri.port)
+response = http.request(Net::HTTP::Get.new(uri.request_uri))
+
+if '302' == response.code
+  puts response['location']
+else
+  warn "# You No Lucky!"
+end
