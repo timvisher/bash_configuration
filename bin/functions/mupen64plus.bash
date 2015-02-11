644c8d98f6f92d49df2698abe0350832d1773cf4
Tim Visher <tvisher@rjmetricss-MacBook-Pro.local>
2015-02-11 09:57:06 -0500

    Allow control of xboxdrv from a function.

--- bin/functions/mupen64plus.bash
+++ bin/functions/mupen64plus.bash
@@ -2,3 +2,15 @@ function n64 {
   local game=$(find ~/Dropbox/ROMs -type f -iname "*$1*")
   mupen64plus --fullscreen --gfx mupen64plus-video-glide64mk2 "$game"
 }
+
+function swap-control-scheme {
+  cp ~/.config/mupen64plus/$1 ~/.config/mupen64plus/mupen64plus.cfg
+}
+
+function mupen64plus-xboxdrv {
+  sudo xboxdrv \
+       --trigger-as-button \
+       --dpad-as-button \
+       --ui-buttonmap guide=KEY_ESC \
+       --axismap x2^deadzone:27000,y2^deadzone:27000 
+}
